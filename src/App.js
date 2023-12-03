0637fd734ed661a0f6ce6b9a96f7a10676865641
quangnavy97 <viktorquang97@gmail.com>
2023-12-03 12:28:40 +0700

    setup pretierrc

--- src/App.js
+++ src/App.js
@@ -3,26 +3,21 @@ import './App.css';
 import Button from './components/Button';
 
 function App() {
-  return (
-    <div className="App">
-        <Button />
+    return (
+        <div className="App">
+            <Button />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
